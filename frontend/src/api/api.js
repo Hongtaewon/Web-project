13ac700420c58406a95c37d2d823fcd4c2c185e6
hongtaewon <xodnjs1245@naver.com>
2020-08-17 13:15:00 +0900

    frontend api 예시 생성 및 redux 적용중..

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,70 @@
 import axios from "axios";
+import APIMessage from "pages/blog/APIMessage";
+import {Storage} from 'api/storage';
 
-const API_URL = "";
+const API_URL = '';
 
-export const getPost = (postId) => axios.get(`/blog/blog-post/${postId}`);
-export const getPosts = (page,size) => axios.get(`/blog/blog-post?page={${page}&size=${size}}`);
-export const writePost = (APIMessage) => axios.post(`/blog/blog-post`, {APIMessage});
+//Post 관련 API
+export const getPost = (postId) => axios.get(`${API_BASE_URL}/blog/blog-post/${postId}`);
+export const getPosts = (page,size) => axios.get(`${API_BASE_URL}/blog/blog-post?page={${page}&size=${size}}`);
+export const writePost = (APIMessage) => axios.post(`${API_BASE_URL}/blog/blog-post`, {APIMessage},
+    {
+        headers: {
+        Authorization: getToken()
+        }
+    }
+);
+export const modifiedPost = (id,APIMessage) => axios.post(`${API_BASE_URL}/blog/blog-post/${id}`,{APIMessage},
+    {
+        headers: {
+        Authorization: getToken()
+        }
+    }
+);
+export const deletePost = (id) => axios.post(`${API_BASE_URL}/blog/blog-post/${id}`,id,
+    {
+        headers: {
+        Authorization: getToken()
+        }
+    }  
+);
+
+//Member 관련 API
+export const login = (APIMessage) => axios.post(`${API_BASE_URL}/Member/login`,{APIMessage});
+export const getMember = () => axios.post(`${API_BASE_URL}/Member/info`,{},
+    {
+        headers: {
+        Authorization: getToken()
+        }
+    }
+);
+export const modifiedMember = (id) => axios.post(`${API_BASE_URL}/member/info`,{},
+    {
+        headers: {
+        Authorization: getToken()
+        }
+    }
+);
+export const deleteMember = (id) => axios.post(`${API_BASE_URL}/member/info`,{},
+    {
+        headers: {
+        Authorization: getToken()
+        }
+    }
+);
+
+//blog 관련 API
+export const getBlog = (id) => axios.get(`${API_BASE_URL}/blog/info/${id}`);
+
+//Home 관련 API
+export const searchBlog = (search) => axios.get(`${API_BASE_URL}/blog/${search}`);
+export const getCategory = () => axios.get(`${API_BASE_URL}/blog/category`);
+export const recommandPost = () => axios.get(`${API_BASE_URL}/blog/blog-post/views`);
+export const recentPost = () => axios.get(`${API_BASE_URL}/blog/blog-post/recent`);
+
+
+const getToken = () => {
+    const token = Storage.local.get('__AUTH__') || Storage.session.get('__AUTH__');
+    
+    return `Bearer ${token}`
+  };
