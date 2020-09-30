675bb5ae18385faa8dea381d460488293f3c2a4d
Ji Yonghyeon <nini98@naver.com>
2020-09-30 15:04:52 +0900

    hikaricp 적용을 위해 backend 제거

--- backend/src/main/java/web/project/backend/repository/JpaCommentRepository.java
+++
@@ -1,9 +0,0 @@
-package web.project.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import web.project.backend.entity.Post_comment;
-
-public interface JpaCommentRepository extends JpaRepository<Post_comment, Long>{
-
-}
